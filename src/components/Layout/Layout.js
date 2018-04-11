a04cf92ac2c3035ed2d523a3ae9a8219f2c3073e
TomIrimescu <tom@irimescu.com>
2018-04-11 17:43:52 -0400

    added Burger, BurgerBuilder, BurgerIngredient components, styling & PropTypes

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,12 @@
 import React from "react";
 
 import Aux from "../../hoc/Auxiliary";
+import classes from "./Layout.css";
 
-const layout  = ( props ) => (
+const layout = props => (
   <Aux>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>
-      {props.children}
-    </main>
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 
