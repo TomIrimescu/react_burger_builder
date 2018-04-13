4f14df614f8cb62909699475dfa4448bac92c176
TomIrimescu <tom@irimescu.com>
2018-04-13 10:54:48 -0400

    changed name from salad to lettuce

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,7 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
 const INGREDIENT_PRICES = {
-  salad: 0.5,
+  lettuce: 0.5,
   cheese: 0.4,
   meat: 1.3,
   bacon: 0.7
@@ -18,7 +18,7 @@ class BurgerBuilder extends Component {
   // }
   state = {
     ingredients: {
-      salad: 0,
+      lettuce: 0,
       bacon: 0,
       cheese: 0,
       meat: 0
@@ -63,7 +63,7 @@ class BurgerBuilder extends Component {
       disabledInfo[key] = disabledInfo[key] <= 0
     }
     console.log(disabledInfo);
-    // {salad: true, bacon: false, cheese; true, meat: false}
+    // {lettuce: true, bacon: false, cheese; true, meat: false}
     
     return (
       <Aux>
