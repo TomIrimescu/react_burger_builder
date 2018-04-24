2facb6c19baf461aa0664ec0762033f31cc1807f
TomIrimescu <tom@irimescu.com>
2018-04-24 11:38:44 -0400

    repaired interceptor response eject

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -22,7 +22,7 @@ const withErrorHandler = ( WrappedComponent, axios ) => {
   
     componentWillUnmount () {
       axios.interceptors.request.eject(this.reqInterceptor);
-      axios.interceptors.request.eject(this.resInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
     
     errorConfirmedHandler = () => {
