97d9d6d837843cd58a174fc34ac6b7bce505135e
TomIrimescu <tom@irimescu.com>
2018-05-04 18:12:09 -0400

    added nested route to ContactData & ContactData form

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+import Button from "../../../components/UI/Button/Button";
+import classes from "./ContactData.css";
+
+class ContactData extends Component {
+  state ={
+    name: "",
+    email: "",
+    address: {
+      street: "",
+      city: "",
+      zipCode: "",
+      country: ""
+    }
+  };
+  
+  render() {
+    return (
+      <div className={classes.ContactData}>
+        <h4>Enter your Contact Data</h4>
+        <form>
+          <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
+          <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
+          <input className={classes.Input} type="text" name="street" placeholder="Your Street Address" />
+          <input className={classes.Input} type="text" name="city" placeholder="Your City" />
+          <input className={classes.Input} type="text" name="zipcode" placeholder="Your Zip Code" />
+          <input className={classes.Input} type="text" name="country" placeholder="Your Country" />
+          <Button btnType="Success">ORDER</Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ContactData;
